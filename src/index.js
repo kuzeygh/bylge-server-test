a418ce0eebb572b870524534a16786f204edafb9
sylmz <soylemezali42@gmail.com>
2019-03-30 01:44:39 +0300

    branch yok

--- src/index.js
+++ src/index.js
@@ -24,3 +24,5 @@ server.start(() => console.log("Server is running on http://localhost:4000"));
 // 4 üncü değişiklikte masteramı gidecek.
 
 // Branch merge edildikten sonra bakalım  değişiklikler nereye gidecek.
+
+// Branchı kaldırdık bakalım nasıl bir sonuç olacak.
